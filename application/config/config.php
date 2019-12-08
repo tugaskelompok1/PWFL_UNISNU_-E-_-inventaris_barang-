03634ae5bad14f9bc27b18d11786e7445a2f131a
Akhmad Toha <56788237+akhmadtoha15@users.noreply.github.com>
2019-12-08 14:44:48 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci_tpa';
+$config['base_url'] = 'http://localhost/ci_tpa/PWFL_UNISNU_-E-_-inventaris_barang-/';
 
 /*
 |--------------------------------------------------------------------------
